9e5085f329f2f6bede6a26faf3df11343554d131
Mariusz Kowalczuk <lordmer@gmail.com>
2019-05-18 12:06:01 +0200

    working

--- src/main/java/pl/dominisz/springintroduction/exception/UnreachableException.java
+++ src/main/java/pl/dominisz/springintroduction/exception/UnreachableException.java
@@ -4,4 +4,8 @@ package pl.dominisz.springintroduction.exception;
  * @author Mariusz Kowalczuk
  */
 public class UnreachableException extends RuntimeException{
+    public UnreachableException(String message) {
+        super(message);
+
+    }
 }
--- src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
+++ src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
@@ -1,15 +1,33 @@
 package pl.dominisz.springintroduction.service;
 
+import pl.dominisz.springintroduction.exception.UnreachableException;
 import pl.dominisz.springintroduction.model.ChargeResult;
 import pl.dominisz.springintroduction.model.CreditCard;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 /**
  * @author Mariusz Kowalczuk
  */
 public class PaypalCreditCardProcessor implements CreditCardProcessor {
+    private Random random = new Random();
+
     public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
-        return null;
+        int number = random.nextInt(3);
+        if (number == 0) {
+            System.out.println("I'm charging card" + creditCard.getNumber() + " for " + amount);
+            return new ChargeResult("Successful charge!", true);
+
+        }
+        else if (number == 1){
+            System.out.println("Charging card" + creditCard.getNumber() + " failed");
+            return new ChargeResult("Limits exceeded", false);
+
+        }
+        else {
+            System.out.println("Connection lost");
+            throw new UnreachableException("Connection lost");
+        }
     }
 }
