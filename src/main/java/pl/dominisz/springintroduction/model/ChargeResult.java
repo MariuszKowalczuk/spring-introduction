b3db04e0eb3027f644fb54e0762c8bcb431e4db9
Mariusz Kowalczuk <lordmer@gmail.com>
2019-05-18 11:07:31 +0200

    charge result

---
+++ src/main/java/pl/dominisz/springintroduction/model/ChargeResult.java
@@ -0,0 +1,22 @@
+package pl.dominisz.springintroduction.model;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Mariusz Kowalczuk
+ */
+
+@AllArgsConstructor
+public class ChargeResult {
+    private String message;
+    private boolean successful;
+
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    public Object getDeclineMessage() {
+        return message;
+    }
+}
