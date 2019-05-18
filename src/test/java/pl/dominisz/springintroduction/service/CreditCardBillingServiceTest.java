4cd1554a2b3e1cd81aa4599c3250e4ec59fe4b96
Mariusz Kowalczuk <lordmer@gmail.com>
2019-05-18 14:13:27 +0200

    tests working

--- src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
+++ src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
@@ -3,6 +3,7 @@ package pl.dominisz.springintroduction.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import pl.dominisz.springintroduction.exception.UnreachableException;
 import pl.dominisz.springintroduction.model.*;
 
 import java.math.BigDecimal;
@@ -31,6 +32,47 @@ class CreditCardBillingServiceTest {
 
         Assertions.assertTrue(receipt.isSuccessful());
         Assertions.assertEquals(new BigDecimal("8.58"), receipt.getAmount());
+    }
+
+    @Test
+    public void shouldDeclinedCharge(){
+        CreditCard creditCard = new CreditCard("Jan", "Kowalski", "123", LocalDate.of(2022, 5, 1));
+        Order order = new Order();
+        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
+        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
+        order.addItem(hotDog);
+        order.addItem(coffee);
+        CreditCardProcessor processor = Mockito.mock(CreditCardProcessor.class);
+        TransactionLog transactionLog = Mockito.mock(TransactionLog.class);
+        CreditCardBillingService creditCardBillingService = new CreditCardBillingService(processor, transactionLog);
+
+        Mockito.when(processor.charge(creditCard, order.getAmount())).thenReturn(new ChargeResult("unsuccesful", false));
+        Receipt receipt = creditCardBillingService.chargeOrder(order, creditCard);
+
+        Assertions.assertFalse(receipt.isSuccessful());
+        Assertions.assertEquals("unsuccesful", receipt.getMessage());
+        Mockito.verify(transactionLog);
+
+    }
+
+    @Test
+    public void shouldFailureCharge(){
+        CreditCard creditCard = new CreditCard("Jan", "Kowalski", "123", LocalDate.of(2022, 5, 1));
+        Order order = new Order();
+        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
+        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
+        order.addItem(hotDog);
+        order.addItem(coffee);
+        CreditCardProcessor processor = Mockito.mock(CreditCardProcessor.class);
+        TransactionLog transactionLog = Mockito.mock(TransactionLog.class);
+        CreditCardBillingService creditCardBillingService = new CreditCardBillingService(processor, transactionLog);
+
+        Mockito.when(processor.charge(creditCard, order.getAmount())).thenThrow(new UnreachableException("connection lost"));
+        Receipt receipt = creditCardBillingService.chargeOrder(order, creditCard);
+
+        Assertions.assertFalse(receipt.isSuccessful());
+        Assertions.assertEquals("connection lost", receipt.getMessage());
+
     }
 
 
